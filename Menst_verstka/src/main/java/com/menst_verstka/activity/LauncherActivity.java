06a9114a1b957d96f6747a62b7c7e8ef83b70e9e
turbo-lover <xaxa1992@gmail.com>
2013-07-29 17:23:39 +0300

    launcher

--- Menst_verstka/src/main/java/com/menst_verstka/activity/LauncherActivity.java
+++ Menst_verstka/src/main/java/com/menst_verstka/activity/LauncherActivity.java
@@ -4,23 +4,38 @@ package com.menst_verstka.activity;/**
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import com.menst_verstka.R;
+import com.menst_verstka.composite.Number_picker;
 
 public class LauncherActivity extends Activity {
 
+    Number_picker cycle;
+    LinearLayout cycle_linear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Initialize_Component();
+        SetComposite();
         SetEventListeners();
     }
 
+
+
+    private void Initialize_Component() {
+        cycle_linear = (LinearLayout) findViewById(R.id.cycle_linear);
+
+    }
+    private void SetComposite() {
+        cycle = new Number_picker(this,1,1,31,"%.0f");
+        cycle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        cycle_linear.addView(cycle);
+    }
     private void SetEventListeners() {
 
     }
 
-    private void Initialize_Component() {
 
-    }
 }
