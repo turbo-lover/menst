7ce7400fcac0315ab7980d63d4f2909d38a32f7e
turbo-lover <xaxa1992@gmail.com>
2013-08-27 18:04:06 +0300

    symptom activity add dynamycly add sympoms element in layout

--- Menst_verstka/src/main/java/com/menst_verstka/activity/SymptomActivity.java
+++ Menst_verstka/src/main/java/com/menst_verstka/activity/SymptomActivity.java
@@ -3,14 +3,14 @@ package com.menst_verstka.activity;/**
  */
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.widget.LinearLayout;
 import com.menst_verstka.R;
 import com.menst_verstka.composite.symptomElement;
 
-public class SympotmActivity extends Activity {
+public class SymptomActivity extends Activity {
 
 
     LinearLayout container;
@@ -29,29 +29,31 @@ public class SympotmActivity extends Activity {
     private void Initialize_Component() {
 
         container = (LinearLayout) findViewById(R.id.symptom_element_container);
+        TypedArray images_array = getResources().obtainTypedArray(R.array.symptoms_icon);
         for(int i = 0, c =0;i<container.getChildCount();i++)
         {
+            LinearLayout column = (LinearLayout) container.getChildAt(i);
             for(int j=0; j< 5; j++, c++)
             {
                 symptomElement s = new symptomElement(this);
                 s.setCurrentValue(1);
-                s.setText(getResources().getTextArray(R.array.symptoms)[c]);
+                s.setText(getResources().getTextArray(R.array.symptoms_text)[c]);
 
                 int width = getResources().getDimensionPixelSize(R.dimen.including_symptom_element);
                 LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(width,0);
+                lp.weight =1;
 
                 s.setLayoutParams(lp);
-                Bitmap b = BitmapFactory.decodeResource(getResources(),R.drawable.appetite);
-                s.setImage(b);
-                LinearLayout column = (LinearLayout) container.getChildAt(i);
+
+                TypedValue typedValue = images_array.peekValue(c);
+                int resourceId = typedValue.resourceId;
+
+                s.setImage(resourceId);
+
                 if (column != null) {
                     column.addView(s);
                 }
             }
         }
-
-
-
-
     }
 }
