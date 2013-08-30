982b12283d4e8713f2d2512a4f62e1f29aaf46b4
Tamlin <spyro@te.ne.ua>
2013-08-30 12:29:15 +0300

    Setting and Launcher

--- Menst_verstka/src/main/java/com/menst_verstka/utils/DBHelper.java
+++ Menst_verstka/src/main/java/com/menst_verstka/utils/DBHelper.java
@@ -8,18 +8,13 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Alexander on 21.08.13.
  */
 public class DBHelper extends SQLiteOpenHelper {
-    public static final String TABLE_NAME1 = "cycle";
-    public static final String TABLE_NAME2 = "params";
-    public static final String CYCLE_START = "start";
-    public static final String CYCLE_END = "end";
+    public static final String TABLE_NAME = "params";
     public static final String DATE = "date";
     public static final String PARAMS = "params";
 
     private static final String DB_NAME = "menstruation";
     private static final int DB_VERSION = 1;
-    private static final String CREATE_TABLE1 = "CREATE TABLE "+TABLE_NAME1+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-            CYCLE_START+" TEXT NOT NULL, "+CYCLE_END+" TEXT NOT NULL);";
-    private static final String CREATE_TABLE2 = "CREATE TABLE "+TABLE_NAME2+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "+
             DATE+" TEXT NOT NULL, "+PARAMS+" TEXT);";
 
     public DBHelper(Context context) {
@@ -29,8 +24,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(CREATE_TABLE1);
-        sqLiteDatabase.execSQL(CREATE_TABLE2);
+        sqLiteDatabase.execSQL(CREATE_TABLE);
     }
 
     @Override
