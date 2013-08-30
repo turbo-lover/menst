44ac3d2ea15d19c2f141a6e600eb3702a6346fbf
turbo-lover <xaxa1992@gmail.com>
2013-08-30 18:19:18 +0300

    start adding functional in DB helper

--- Menst_verstka/src/main/java/com/menst_verstka/utils/DBHelper.java
+++ Menst_verstka/src/main/java/com/menst_verstka/utils/DBHelper.java
@@ -1,21 +1,37 @@
 package com.menst_verstka.utils;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 /**
  * Created by Alexander on 21.08.13.
+ * All necessary method are presented here;
  */
 public class DBHelper extends SQLiteOpenHelper {
-    public static final String TABLE_NAME = "params";
-    public static final String DATE = "date";
-    public static final String PARAMS = "params";
+    /* * * * * * * * * * * *  Tables * * * * * * * * * * * */
+    private static final String TABLE_CYCLE = "cycle";
+    private static final String TABLE_PARAMS = "params";
+    /* * * *  * * * * * * * *  Columns of TABLE_CYCLE * * * * * * * * * * * */
+    private static final String CYCLE_START = "start";
+    private static final String CYCLE_END = "end";
+    /* * * *  * * * * * * * *  Columns of TABLE_PARAMS * * * * * * * * * * * */
+    private static final String DATE = "date";
+    private static final String PARAMS = "params";
+    /* * * * * * * * * * * *  FIELDS OF JSON OBJ * * * * * * * * * * * */
+
 
     private static final String DB_NAME = "menstruation";
     private static final int DB_VERSION = 1;
-    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-            DATE+" TEXT NOT NULL, "+PARAMS+" TEXT);";
+    private static final String CREATE_TABLE1 = "CREATE TABLE "+TABLE_CYCLE+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + CYCLE_START+" TEXT NOT NULL, "+CYCLE_END+" TEXT NOT NULL);";
+    private static final String CREATE_TABLE2 = "CREATE TABLE "+ TABLE_PARAMS +
+            " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +DATE+" TEXT NOT NULL, "+PARAMS+" TEXT);";
 
     public DBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -24,9 +40,48 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(CREATE_TABLE);
+        sqLiteDatabase.execSQL(CREATE_TABLE1);
+        sqLiteDatabase.execSQL(CREATE_TABLE2);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) { }
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+
+    /**
+     *
+     * @param date
+     * @return com.google.gson.JsonObject that contain all information about
+     * this date;
+     */
+    private JsonObject getRawJsonByDate (String date){
+        SQLiteDatabase db = null;
+        Cursor cursor  = null;
+
+        try {
+            //try to get readableDatabase;
+            db = getReadableDatabase();
+
+            if (db != null) {
+                // select $PARAMS from table $TABLE_PARAMS where $DATE == $date
+                cursor = db.query(TABLE_PARAMS, new String[]{PARAMS}, DATE + "=?", new String[]{date}, null, null, null);
+
+                cursor.moveToFirst();
+            }
+
+            return new JsonParser().parse(cursor != null ? cursor.getString(0) : null).getAsJsonObject();
+
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            if(db !=null)
+            db.close();
+        }
+        return null;
+    }
 }
+
+
+
+
