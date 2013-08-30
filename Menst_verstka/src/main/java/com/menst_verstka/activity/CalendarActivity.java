f1f8fe54391e96adef9f1a067d35fef5dc5b63e1
Tamlin <spyro@te.ne.ua>
2013-08-30 12:29:35 +0300

    Calendar Activity

--- Menst_verstka/src/main/java/com/menst_verstka/activity/CalendarActivity.java
+++ Menst_verstka/src/main/java/com/menst_verstka/activity/CalendarActivity.java
@@ -2,12 +2,17 @@ package com.menst_verstka.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.menst_verstka.R;
+import com.menst_verstka.composite.compositeCalendar;
+import com.menst_verstka.composite.compositeSettings;
 import com.menst_verstka.composite.month;
+import com.menst_verstka.utils.frameActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,39 +21,19 @@ import java.util.Date;
 /**
  * Created by Alexander on 27.08.13.
  */
-public class CalendarActivity extends Activity {
-    private LinearLayout months;
+public class CalendarActivity extends frameActivity implements View.OnClickListener {
+    private compositeCalendar cmpCalendar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        InitializeComponent();
-        SetCompositeElements();
-        SetEventListeners();
+    protected void InitializeComponent() {
+        super.InitializeComponent();
+        cmpCalendar = new compositeCalendar(this);
     }
 
-    private void SetEventListeners() {
-
+    @Override
+    protected void SetCompositeElements() {
+        HideNavBar();
+        SetHeaderText(getString(R.string.calendar_title));
+        content.addView(cmpCalendar);
     }
-
-    private void SetCompositeElements() {
-
-    }
-
-    private void InitializeComponent() {
-        setContentView(R.layout.calendar);
-        months = (LinearLayout) findViewById(R.id.months_holder);
-        month m1 = new month(this);
-        month m2 = new month(this);
-        month m3 = new month(this);
-        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0,1f);
-        m1.SetMonth(2013,7);
-        m2.SetMonth(2013,8);
-        m3.SetMonth(2013,9);
-        months.addView(m1,p);
-        months.addView(m2,p);
-        months.addView(m3,p);
-    }
-
-
 }
